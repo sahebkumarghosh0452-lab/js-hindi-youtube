58761ceee5750e0705689e502398fcbc4a221c74
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-01-16 00:14:06 +0530

    Refactor string logging in 05_strings.js
    Commented out old console.log statement for clarity.

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,13 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); - old way  
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
+// 
+
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
