64ce6068128f87db46f5ec4cb81592bfa3f2a7b1
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-01-15 22:55:12 +0530

    Update comparison logs for null and undefined values
    Clarified comparison results for null and undefined.

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,14 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true : cuz the '>' operator convert null to 0 
 
-console.log(undefined == 0);
+console.log(undefined == 0); // false in all cases 
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === - Strict Check 
 
-console.log("2" === 2);
+console.log("2" === 2);//it also checks the datatyoe of the variable 
