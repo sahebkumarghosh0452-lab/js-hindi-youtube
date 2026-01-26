9d566b4f8e88a9d35de0eb5de2014d06ad330d66
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-01-26 20:05:10 +0530

    Improve comments in 01_arrays.js for array methods
    Updated comments for clarity and added explanations for array methods.

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,24 +1,24 @@
-// array
+// array - DYNAMIC IN NATURE IN JAVASCRIPT 
 
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1, 2, 3, 4)
+const myArr2 = new Array(1, 2, 3, 4) // prototype under prototype 
 // console.log(myArr[1]);
 
 // Array methods
 
 // myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// myArr.push(7) - add a element at last 
+// myArr.pop() - delete value at the last 
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) - add element in start of the array 
+// myArr.shift() - remopve first element of the array
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() - convert to string comma separated 
 
 // console.log(myArr);
 // console.log( newArr);
@@ -28,12 +28,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // it donot convert the array the original array remain same after the operation 
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // it alter the original array cut it into valid substring that is givenit includes the range given
 console.log("C ", myArr);
 console.log(myn2);
